b980edb771e0f62ade63a7a13d7107284a1cf7d5
Piyush Srivastava <psrivastava@digitalocean.com>
2022-04-26 20:31:13 -0700

    Use xo/dburl.

--- packages/ts/hello/go.mod
+++ packages/ts/hello/go.mod
@@ -2,4 +2,7 @@ module github.com/piyush0101/sample-dbaas
 
 go 1.17
 
-require github.com/go-sql-driver/mysql v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/xo/dburl v0.9.1
+)
--- packages/ts/hello/main.go
+++ packages/ts/hello/main.go
@@ -1,36 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/xo/dburl"
 )
 
 // Main is the entry point of function.
 func Main(args map[string]interface{}) map[string]interface{} {
 	res := make(map[string]interface{})
-	dbURL := os.Getenv("DATABASE_URL")
-	res["body"] = dbURL
+	db, err := dburl.Open(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		res["body"] = err.Error()
+		return res
+	}
+	defer db.Close()
+	r := db.QueryRow("SELECT 1 FROM DUAL")
+	if err != nil {
+		fmt.Println("could not query from db")
+		res["body"] = err.Error()
+		return res
+	}
+	var result uint32
+	err = r.Scan(&result)
+	if err != nil {
+		res["body"] = err.Error()
+		return res
+	}
+	res["body"] = result
 	return res
-	// db, err := sql.Open("mysql", dbURL)
-	// defer db.Close()
-	// if err != nil {
-	// 	fmt.Println("could not connect to database")
-	// 	res["body"] = dbURL
-	// 	return res
-	// }
-	// r := db.QueryRow("SELECT 1 FROM DUAL")
-	// if err != nil {
-	// 	fmt.Println("could not query from db")
-	// 	res["body"] = err.Error()
-	// 	return res
-	// }
-	// var result uint32
-	// err = r.Scan(&result)
-	// if err != nil {
-	// 	res["body"] = err.Error()
-	// 	return res
-	// }
-	// res["body"] = result
-	// return res
 }
